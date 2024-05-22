5b538647ddc61ce5c4a39f8ba5fc6d5b975dc637
Markus Mayer <mk@mayermarkus.net>
2024-05-22 22:33:37 +0200

    fix: improve middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,10 @@ const randomNumberBetween = (min: number, max: number) =>
   Math.floor(Math.random() * (max - min + 1) + min)
 
 export function middleware(request: NextRequest) {
-  return NextResponse.rewrite(new URL(`/${randomNumberBetween(1, 16)}`, request.url))
+  const url = new URL(request.url)
+  if (url.pathname === '/') return NextResponse.rewrite(new URL(`/prerendered/${randomNumberBetween(1, 16)}`, request.url))
+  
+  return NextResponse.next()
 }
 
 export const config = {
